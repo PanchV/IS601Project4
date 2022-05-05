5eecd31bb80d2e3d14fc8d444d70b8c9c3377ce6
PanchV <32369823+PanchV@users.noreply.github.com>
2022-05-05 19:43:46 -0400

    Transactions Additions

--- app/cli/__init__.py
+++ app/cli/__init__.py
@@ -1,9 +1,10 @@
 import click
 from flask.cli import with_appcontext
 from app.db import db
-
+from app.db import make_db_directory
 
 @click.command(name='create-db')
 @with_appcontext
 def create_database():
+    make_db_directory()
     db.create_all()
