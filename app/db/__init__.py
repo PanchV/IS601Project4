73f3fa74d5edce08b99b93ee6e38f89f55303597
PanchV <32369823+PanchV@users.noreply.github.com>
2022-05-05 19:40:21 -0400

    DB logging

--- app/db/__init__.py
+++ app/db/__init__.py
@@ -1,5 +1,9 @@
 from flask import Blueprint, cli
 from flask_sqlalchemy import SQLAlchemy
+from app import config
+
+import logging
+import os
 
 db = SQLAlchemy()
 
@@ -8,3 +12,12 @@ database = Blueprint('database', __name__,)
 @database.cli.command('create')
 def init_db():
     db.create_all()
+
+
+def make_db_directory():
+    log = logging.getLogger("misc_debug")
+    root = current_app.config["BASE_DIR"]
+    db_dir = os.path.join(root, '..', current_app.config["DB_DIR"])
+    if not os.path.exists(db_dir):
+        log.debug("[%s] Making DB Directory %s", current_app.env, db_dir)
+        os.mkdir(db_dir)
