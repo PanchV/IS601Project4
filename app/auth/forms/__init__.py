67cc378716ae4031c5a6b5759402ee2c82cc14b1
PanchV <32369823+PanchV@users.noreply.github.com>
2022-05-09 10:14:43 -0400

    Welcome page

--- app/auth/forms/__init__.py
+++ app/auth/forms/__init__.py
@@ -1,3 +1,5 @@
+
+
 from flask_wtf import FlaskForm
 from wtforms import validators
 from wtforms.fields import *
@@ -31,14 +33,26 @@ class register_form(FlaskForm):
 
 
 class profile_form(FlaskForm):
+
     about = TextAreaField('About', [validators.length(min=6, max=300)],
                           description="Please add information about yourself")
-
+    hobbies = TextAreaField('Hobbies', [validators.length(min=6, max=300)],
+                          description="Please add information about your hobbies")
+    education = TextAreaField('Education', [validators.length(min=6, max=300)],
+                          description="Please add information about your education")
+    skills = TextAreaField('Skills', [validators.length(min=6, max=300)],
+                          description="Please add information about your skills")
     submit = SubmitField()
 
 class user_edit_form(FlaskForm):
-    about = TextAreaField('About', [validators.length(min=6, max=300)],
-                          description="Please add information about yourself")
+    about = TextAreaField('About', [validators.length(min=0, max=300)],
+                          description="You may edit About information the user as an Admin")
+    hobbies = TextAreaField('Hobbies', [validators.length(min=0, max=300)],
+                            description="You may edit Hobbies information the user as an Admin")
+    education = TextAreaField('Education', [validators.length(min=0, max=300)],
+                              description="You may edit Education information the user as an Admin")
+    skills = TextAreaField('Skills', [validators.length(min=0, max=300)],
+                           description="You may edit Skills information the user as an Admin")
     is_admin = BooleanField('Admin', render_kw={'value':'1'})
     submit = SubmitField()
 
@@ -56,4 +70,8 @@ class security_form(FlaskForm):
     ], description="Create a password ")
     confirm = PasswordField('Repeat Password', description="Please retype your password to confirm it is correct")
 
+    submit = SubmitField()
+
+class csv_upload(FlaskForm):
+    file = FileField()
     submit = SubmitField()
