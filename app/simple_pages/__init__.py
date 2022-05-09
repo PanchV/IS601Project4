6c2b50553a8d359ab9cd3e46e177314e8e3793fb
PanchV <32369823+PanchV@users.noreply.github.com>
2022-05-09 10:52:11 -0400

    form updates

--- app/simple_pages/__init__.py
+++ app/simple_pages/__init__.py
@@ -1,23 +1,41 @@
-""" Render Simple Pages"""
 from flask import Blueprint, render_template, abort
 from jinja2 import TemplateNotFound
-from app.auth.forms import login_form
 
-simple_pages = Blueprint('simple_pages', __name__, template_folder='templates')
+import logging
 
-# @simple_pages.route('/')
-# def index():
-#     """ root index.html """
-#     try:
-#         #return render_template('index.html', form=login_form())
-#     except TemplateNotFound:
-#         abort(404)
+simple_pages = Blueprint('simple_pages', __name__,
+                        template_folder='templates')
 
+
+@simple_pages.route('/')
+def index():
+    try:
+        log = logging.getLogger("eachRequestResponse")
+        log.info("opened home page")
+        return render_template('index.html')
+    except TemplateNotFound:
+        log = logging.getLogger("eachRequestResponse")
+        log.info("home page 404")
+        abort(404)
 
 @simple_pages.route('/about')
 def about():
-    """ about.html """
     try:
+        log = logging.getLogger("eachRequestResponse")
+        log.info("opened about page")
         return render_template('about.html')
     except TemplateNotFound:
+        log = logging.getLogger("eachRequestResponse")
+        log.info("about page 404")
+        abort(404)
+
+@simple_pages.route('/welcome')
+def welcome():
+    try:
+        log = logging.getLogger("eachRequestResponse")
+        log.info("opened welcome page")
+        return render_template('welcome.html')
+    except TemplateNotFound:
+        log = logging.getLogger("eachRequestResponse")
+        log.info("welcome page 404")
         abort(404)
