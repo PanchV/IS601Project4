5bbf18eb402cba60d9d9a9b1b65a3605799dd3f8
PanchV <32369823+PanchV@users.noreply.github.com>
2022-05-09 09:07:53 -0400

    Forms

--- app/__init__.py
+++ app/__init__.py
@@ -1,92 +1,160 @@
-import os
-import flask
-from flask import Flask
-from flask import render_template
-from flask_bootstrap import Bootstrap5 # pylint: disable=no-name-in-module
-from flask_cors import CORS
+"""A simple flask web app"""
 import flask_login
+import os
+import datetime
+import time
+
+from flask import g, request
+from rfc3339 import rfc3339
+
+from flask import render_template, Flask, has_request_context, request
+from flask_bootstrap import Bootstrap5
 from flask_wtf.csrf import CSRFProtect
+
 from app.auth import auth
-from app.cli import create_database
-from app.db import database, db
-from app.db.models import User
-from app.simple_pages import simple_pages
+from app.auth import auth
 from app.transactions import transactions
-from app.logger_config import log_conf
+from app.cli import create_database
 from app.context_processor import utility_context_processor
+from app.db import db,database
+
+from app.db.models import User
+from app.exceptions import http_exceptions
+from app.simple_pages import simple_pages
+import logging
+from flask.logging import default_handler
 
 login_manager = flask_login.LoginManager()
 
 
 def page_not_found(e):
-    """ handle 404 """
-    # pylint: disable=invalid-name
-    # TODO log e when logging implemented
     return render_template("404.html"), 404
+
+
+class RequestFormatter(logging.Formatter):
+    def format(self, record):
+        if has_request_context():
+            record.url = request.url
+            record.remote_addr = request.remote_addr
+        else:
+            record.url = None
+            record.remote_addr = None
+
+        return super().format(record)
 
 
 def create_app():
     """Create and configure an instance of the Flask application."""
+
     app = Flask(__name__)
-
-    # https://pythonhosted.org/Flask-WTF/csrf.html
-    CSRFProtect(app)
-
-    #----------------------------------------
-    # basic config
-    #
-    if  os.environ.get("FLASK_ENV") == "production":
-        app.config.from_object("app.config.ProductionConfig")
-    elif os.environ.get("FLASK_ENV") == "development":
-        app.config.from_object("app.config.DevelopmentConfig")
-    elif os.environ.get("FLASK_ENV") == "testing":
-        app.config.from_object("app.config.TestingConfig")
-
-    app.context_processor(utility_context_processor)
-    app.register_blueprint(log_conf)
-
-    #----------------------------------------
-    # web stuff
-    #
-
-    # https://flask-login.readthedocs.io/en/latest/  <-login manager
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    app.secret_key = 'This is an INSECURE secret!! DO NOT use this in production!!'
     login_manager.init_app(app)
     login_manager.login_view = "auth.login"
-
-    # Needed for CSRF protection of form submissions and WTF Forms
-    # https://wtforms.readthedocs.io/en/3.0.x/
-
-
-    # setup models/database
-    app.register_blueprint(database)
-
-    # load routes/webpages
-    bootstrap = Bootstrap5(app) # pylint: disable=unused-variable
-    app.register_error_handler(404, page_not_found)
+    csrf = CSRFProtect(app)
+    bootstrap = Bootstrap5(app)
     app.register_blueprint(simple_pages)
     app.register_blueprint(auth)
+    app.register_blueprint(database)
+    app.context_processor(utility_context_processor)
     app.register_blueprint(transactions)
-
-
+    app.config['BOOTSTRAP_BOOTSWATCH_THEME'] = 'Simplex'
+    app.register_error_handler(404, page_not_found)
+    # app.add_url_rule("/", endpoint="index")
+    DB_DIR = os.getenv('DB_DIR','database')
+    app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///" + os.path.join(BASE_DIR, '..', DB_DIR, "db.sqlite")
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.init_app(app)
-    api_v1_cors_config = {
-        "methods": ["OPTIONS", "GET", "POST"],
-    }
-    CORS(app, resources={"/api/*": api_v1_cors_config})
 
-    #----------------------------------------
     # add command function to cli commands
-    #
     app.cli.add_command(create_database)
+
+    # Deactivate the default flask logger so that log messages don't get duplicated
+    app.logger.removeHandler(default_handler)
+
+    # get root directory of project
+    root = os.path.dirname(os.path.abspath(__file__))
+
+    # set the name of the transaction uploads folder
+    upload_dir = os.path.join(root, 'uploads')
+    # make a directory if it doesn't exist
+    if not os.path.exists(upload_dir):
+        os.mkdir(upload_dir)
+
+    # set the name of the apps log folder to logs
+    logdir = os.path.join(root, 'logs')
+    # make a directory if it doesn't exist
+    if not os.path.exists(logdir):
+        os.mkdir(logdir)
+    # set name of the log file
+    log_file = os.path.join(logdir, 'info.log')
+
+    handler = logging.FileHandler(log_file)
+    # Create a log file formatter object to create the entry in the log
+    formatter = RequestFormatter(
+        '[%(asctime)s] %(remote_addr)s requested %(url)s\n'
+        '%(levelname)s in %(module)s: %(message)s'
+    )
+    # set the formatter for the log entry
+    handler.setFormatter(formatter)
+    # Set the logging level of the file handler object so that it logs INFO and up
+    handler.setLevel(logging.INFO)
+    # Add the handler for the log entry
+    app.logger.addHandler(handler)
+
+    @app.before_request
+    def start_timer():
+        g.start = time.time()
+
+    @app.after_request
+    def log_request(response):
+        if request.path == '/favicon.ico':
+            return response
+        elif request.path.startswith('/static'):
+            return response
+        elif request.path.startswith('/bootstrap'):
+            return response
+
+        now = time.time()
+        duration = round(now - g.start, 2)
+        dt = datetime.datetime.fromtimestamp(now)
+        timestamp = rfc3339(dt, utc=True)
+
+        ip = request.headers.get('X-Forwarded-For', request.remote_addr)
+        host = request.host.split(':', 1)[0]
+        args = dict(request.args)
+
+        log_params = [
+            ('method', request.method),
+            ('path', request.path),
+            ('status', response.status_code),
+            ('duration', duration),
+            ('time', timestamp),
+            ('ip', ip),
+            ('host', host),
+            ('params', args)
+        ]
+
+        request_id = request.headers.get('X-Request-ID')
+        if request_id:
+            log_params.append(('request_id', request_id))
+
+        parts = []
+        for name, value in log_params:
+            part = name + ': ' + str(value) + ', '
+            parts.append(part)
+        line = " ".join(parts)
+        #this triggers a log entry to be created with whatever is in the line variable
+        app.logger.info('this is the plain message')
+
+        return response
 
     return app
 
 
 @login_manager.user_loader
 def user_loader(user_id):
-    """  callback function """
-
     try:
         return User.query.get(int(user_id))
-    except: # pylint: disable=bare-except
+    except:
         return None
